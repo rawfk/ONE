2ab9e2886034bc353e4e5b8140fbb4e8de059366
ToToRo_1106 <132899279+Junghwan1106@users.noreply.github.com>
2023-10-10 19:51:29 +0900

    Add files via upload

---
+++ main/main.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    $('#fullpage').fullpage({
+		//options here
+        anchors:['section1', 'section2', 'section3', 'section4', 'section5'],
+        navigation: true,
+        navigationPosition: 'right',
+        // sectionsColor: ['#fff', '#fff', '#fff', '#fff', '#fff'],
+	});
+});
